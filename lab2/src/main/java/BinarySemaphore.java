a91a16da689777e4b3876b86e77246736172234b
dak98 <jakub.czajka1998@gmail.com>
2019-10-09 15:57:59 +0200

    Corrected the implementation of P() and V(). Added additional comments

--- lab2/src/main/java/BinarySemaphore.java
+++ lab2/src/main/java/BinarySemaphore.java
@@ -1,15 +1,20 @@
 public class BinarySemaphore {
+    /**
+     * This variable can have one of two values: 0 or 1 (the default). Value 0
+     * means that the semaphore is taken and any thread which wants to use it
+     * must wait.
+     */
     private int value;
 
     public BinarySemaphore() {
-        value = 0;
+        value = 1;
     }
 
     /**
-     * Increments the value of the semaphore. Blocks if it is already 1.
+     * Decrements the value of the semaphore. Blocks if it is already 1.
      */
     public synchronized void P() {
-        while (value == 1) {
+        while (value == 0) {
             try {
                 this.wait();
             } catch (InterruptedException e) {
@@ -21,10 +26,10 @@ public class BinarySemaphore {
     }
 
     /**
-     * Decrements the value of the semaphore. Blocks if it is already 0.
+     * Increments the value of the semaphore. Blocks if it is already 0.
      */
     public synchronized void V() {
-        while (value == 0) {
+        while (value == 1) {
             try {
                 this.wait();
             } catch (InterruptedException e) {
