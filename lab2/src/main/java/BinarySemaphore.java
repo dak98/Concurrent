7d98918bed973d4216a35628f6964e570d4d5f50
dak98 <jakub.czajka1998@gmail.com>
2019-10-09 15:42:32 +0200

    Implemented the BinarySemaphore

--- lab2/src/main/java/BinarySemaphore.java
+++ lab2/src/main/java/BinarySemaphore.java
@@ -1,2 +1,37 @@
 public class BinarySemaphore {
+    private int value;
+
+    public BinarySemaphore() {
+        value = 0;
+    }
+
+    /**
+     * Increments the value of the semaphore. Blocks if it is already 1.
+     */
+    public synchronized void P() {
+        while (value == 1) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        value = 0;
+        notifyAll();
+    }
+
+    /**
+     * Decrements the value of the semaphore. Blocks if it is already 0.
+     */
+    public synchronized void V() {
+        while (value == 0) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        value = 1;
+        notifyAll();
+    }
 }
