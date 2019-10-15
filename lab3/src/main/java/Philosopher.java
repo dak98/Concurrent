9a049fcdd07625a07b5a87a835ae19d192774be5
dak98 <jakub.czajka1998@gmail.com>
2019-10-15 20:07:15 +0200

    Added a method to collect average wait time of a philosopher

--- lab3/src/main/java/Philosopher.java
+++ lab3/src/main/java/Philosopher.java
@@ -1,14 +1,24 @@
+import java.util.LinkedList;
 import java.util.concurrent.Semaphore;
 
-public class Philosopher extends Thread {
-    protected final int id;
-    protected Semaphore leftFork, rightFork, table;
 
-    public Philosopher(int id, Semaphore leftFork, Semaphore rightFork,
-                       Semaphore table) {
+class Philosopher extends Thread {
+    protected int id;
+    protected Semaphore leftFork, rightFork;
+    protected LinkedList<Long> timestamps = new LinkedList<>();
+
+    public Philosopher(int id, Semaphore leftFork, Semaphore rightFork) {
         this.id = id;
         this.leftFork = leftFork;
         this.rightFork = rightFork;
-        this.table = table;
+    }
+
+    public double getAverageWaitTime() {
+        double averageWaitTime = 0.0;
+        for (int i = 0; i < timestamps.size() - 1; i++) {
+            long duration = (timestamps.get(i + 1) - timestamps.get(i));
+            averageWaitTime += duration / 1000000000.0 - 2;
+        }
+        return averageWaitTime / timestamps.size() + 2;
     }
 }
